8d1ee687a4ca551d4ab404747402be287a2044ae
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-28 09:38:51 -0500

    exercise 11 completed

--- script.js
+++ script.js
@@ -116,30 +116,42 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
     // console.log(pizzaStatus)
 
 // EXERCISE 10
-    // 1. SET LANGUAGE
+    // // 1. SET LANGUAGE
 
-    // Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+    // // Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
 
-    // "LANG is equal to localLangConfig or the default value of English."
+    // // "LANG is equal to localLangConfig or the default value of English."
 
-    const localLangConfig = 'fr';  // Change to 'es', 'fr', etc., or keep it null
+    // const localLangConfig = 'fr';  // Change to 'es', 'fr', etc., or keep it null
 
-    // a. Create a variable called LANG
-    // b. Assign LANG the value of localLangConfig or 'en' as a default
+    // // a. Create a variable called LANG
+    // // b. Assign LANG the value of localLangConfig or 'en' as a default
 
-    // Your code here
-    const lang = 'en' || localLangConfig
-    // Log the result
-    console.log('Language setting:', lang);
+    // // Your code here
+    // const lang = 'en' || localLangConfig
+    // // Log the result
+    // console.log('Language setting:', lang);
 
-    // 2. SET WEBSITE THEME
+    // // 2. SET WEBSITE THEME
 
-    const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+    // const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
 
-    // a. Create a variable called USER_THEME
-    // b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+    // // a. Create a variable called USER_THEME
+    // // b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
 
-    // Your code here
-    const user_theme = 'light' || userSavedTheme
-    // Log the result
-    console.log('User theme setting:', user_theme);
+    // // Your code here
+    // const user_theme = 'light' || userSavedTheme
+    // // Log the result
+    // console.log('User theme setting:', user_theme);
+
+// EXERCISE 11
+    // Now check for `cat.age` on `adventurer`. See how it errors out?
+    //Use optional chaining in a console.log that it returns undefined instead.
+
+    const adventurer = {
+        name: 'Alice',
+    };
+    
+    let cat = adventurer.cat?.name
+    
+    console.log(cat)
